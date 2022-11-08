448020fad6fae6c98c826ce07e01340af3bba74a
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-08 14:01:41 +0600

    for specific data load using id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -24,13 +24,25 @@ async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
 
-        app.get('/services',async(req, res) => {
+        app.get('/services', async (req, res) => {
             
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
+      
+         // for getting specific id
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+            
+           
+
+        } )
 
     }
     finally {
