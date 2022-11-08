5b8630a5eed9fd49d9f1f3427c426f0f6ff783f7
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-08 12:40:59 +0600

    basic node mongo setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
  
@@ -7,6 +9,23 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// password : 2g4e1r3r82rqVu5M
+//userName: geniusDBUser
+
+ 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yagkdpa.mongodb.net/?retryWrites=true&w=majority`;
+
+ 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/', (req, res) => {
